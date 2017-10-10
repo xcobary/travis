3afbadfb4751d770929d7ae0dc41c36aca7da6f3
Jerry James A <call08050208030@yahoo.com>
2017-10-10 07:33:53 +0100

    -minit commit

---
+++ database/migrations/2017_10_09_234806_create_events_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('address');
+            $table->float('lat', 10,6);
+            $table->float('long', 10,6);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
